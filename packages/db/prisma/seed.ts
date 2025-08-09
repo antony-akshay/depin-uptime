f45a7686aff32ba8dd978ff31a859c1c1e280392
antony-akshay <akshay.dev.ac.in@gmail.com>
2025-08-09 21:53:32 +0530

    db

--- packages/db/prisma/seed.ts
+++ packages/db/prisma/seed.ts
@@ -1,53 +1,62 @@
-import { PrismaClient } from "@prisma/client/extension";
-import { prismaClient } from "./src";
+import "dotenv/config";
+import { prismaClient } from "../src";
+
+const USER_ID = "4";
 
 async function seed() {
     await prismaClient.user.create({
         data: {
-            id: "1",
+            id: USER_ID,
             email: "test@test.com",
-            password: "test",
+
         }
     })
 
-    await prismaClient.website.create({
+    const website = await prismaClient.website.create({
         data: {
-            id: "1",
+            id: USER_ID,
             url: "http://google.com",
             userId: "1"
         }
     })
 
+    const validator = await prismaClient.validator.create({
+        data: {
+            publicKey: "0x1212121121",
+            location: "Delhi",
+            ip: "127.0.0.1"
+        }
+    })
+
     await prismaClient.websiteTick.create({
         data: {
-            id: "1",
-            websiteId: "1",
+            websiteId: website.id,
             status: "Good",
             createdAt: new Date(),
             latency: 100,
-            validatorId: "1"
+            validatorId: validator.id
         }
     })
 
     await prismaClient.websiteTick.create({
         data: {
-            id: "1",
-            websiteId: "1",
+            websiteId: website.id,
             status: "Good",
             createdAt: new Date(Date.now() - 1000 * 60 * 10),
             latency: 100,
-            validatorId: "1"
+            validatorId: validator.id
         }
     })
 
     await prismaClient.websiteTick.create({
         data: {
-            id: "1",
-            websiteId: "1",
-            status: "Good",
+            websiteId: website.id,
+            status: "Bad",
             createdAt: new Date(Date.now() - 1000 * 60 * 20),
             latency: 100,
-            validatorId: "1"
+            validatorId: validator.id
         }
     })
 }
+
+seed();
